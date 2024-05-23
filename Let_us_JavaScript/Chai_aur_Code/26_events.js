12b24c36a74166ddb896dcf28670a9f70fb43eb9
Manoj Kumar <156507459+manoj868594@users.noreply.github.com>
2024-05-23 17:53:16 +0530

    Update 26_events.js

--- Let_us_JavaScript/Chai_aur_Code/26_events.js
+++ Let_us_JavaScript/Chai_aur_Code/26_events.js
@@ -1,25 +1,25 @@
 const sayHitesh = function () {
     console.log("JS Course");
-  };
+};
   // setTimeout(sayHitesh, 2000);
   // sayHitesh() in block is execut and refrence sayhitesh only
-  const changeText = function () {
-    document.querySelector("h1").innerHTML = "Best JS Series";
+const changeText = function () {
+  document.querySelector("h1").innerHTML = "Best JS Series";
     // console.log("changged")
-  };
+};
   // const changeMe = setTimeout(changeText, 2000);
 
-  document.querySelector("#stop").addEventListener("click", clearInt);
-  function clearInt() {
+document.querySelector("#stop").addEventListener("click", clearInt);
+function clearInt() {
     // clearTimeout(changeMe)
     // console.log("STOPPED")
-  }
+}
 
-  // const strh2 = document.querySelector("h2");
-  // const start = document.querySelector("#start1").addEventListener("click", startInterval);
-  // const stop = document.querySelector("#stop1").addEventListener("click", stopInterval);
+const strh2 = document.querySelector("h2");
+const start = document.querySelector("#start1").addEventListener("click", startInterval);
+const stop = document.querySelector("#stop1").addEventListener("click", stopInterval);
 
-  function startInterval() {
+function startInterval() {
       intervalId = setInterval(function () {
           updateDateTime();
         }, 1000);
@@ -51,10 +51,40 @@ const randomColor = function() {
 }
 // console.log(randomColor());
 // console.log(Math.floor(Math.random() * 16))
-
-const startChangingColor = function(){}
-const stopChangingColor = function(){}
+let intervalID;
+const startChangingColor = function(){
+  if(!intervalID){
+    intervalID = setInterval(changeBg, 1000);
+  }
+  function changeBg (){
+    document.body.style.backgroundColor = randomColor();
+  }
+}
+const stopChangingColor = function(){
+  clearInterval(intervalID);
+  intervalID = null;
+}
 document.querySelector('#startbg').addEventListener('click', startChangingColor);
 document.querySelector('#stopbg').addEventListener('click', stopChangingColor);
 
-39 video 
+/**********Keyboard key press event************/
+const insert = document.getElementById('insert');
+
+window.addEventListener('keydown', (e)=> {
+  insert.innerHTML = `
+    <div class="class">
+      <table>
+        <tr>
+          <th>key</th>
+          <th>Keycode</th>
+          <th>Code</th>
+        </tr>
+        <tr>
+          <td>${e.key === ' ' ? "space" : e.key}</td>
+          <td>${e.keyCode}</td>
+          <td>${e.code}</td>
+        </tr>
+      </table>
+    </div>
+  `;
+})
